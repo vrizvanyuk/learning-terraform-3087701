810baff38936ba71792b50e14b60ed67fa5deac5
vrizvanyuk <155646798+vrizvanyuk@users.noreply.github.com>
2024-01-05 03:13:43 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -30,34 +30,10 @@ output "common_tags" {
   value = var.common_tags
 }
 
-output "ami_id" {
-  value = var.ami_id
-}
-
 output "instance_count" {
   value = var.instance_count
 }
 
-output "bucket_name" {
-  value = var.bucket_name
-}
-
 output "rds_instance_class" {
   value = var.rds_instance_class
-}
-
-output "eip_allocation_id" {
-  value = var.eip_allocation_id
-}
-
-output "security_group_ids" {
-  value = var.security_group_ids
-}
-
-output "iam_role_name" {
-  value = var.iam_role_name
-}
-
-output "route_table_id" {
-  value = var.route_table_id
 }
