f3af517dca151c1b6d5c2b08c4b308e3c32a1250
vrizvanyuk <155646798+vrizvanyuk@users.noreply.github.com>
2024-01-05 02:38:44 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,12 +14,6 @@ variable "subnet_cidrs" {
     default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "subnet_cidrs" {
-    description = "List of CIDR blocks for subnets"
-    type        = list(string)
-    default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
 variable "key_name" {
     description = "The key name to use for the instance"
     default     = "my-aws-key"
